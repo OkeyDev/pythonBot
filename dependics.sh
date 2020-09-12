b425644f1a2ebf2b580371ee5d3d255ae2491dde
OkeyDev <dom33m3@gmail.com>
2020-09-12 17:55:53 +0300

    Update dependics.sh

--- dependics.sh
+++ dependics.sh
@@ -1,10 +1,10 @@
-echo -e "\e[31mPlease start this script once. Don't go out from your pc, we need you to write a password \e[39m"
+echo -e "\e[31mPlease start this script once. Don't disable your pc \e[39m"
 sudo apt-get update
 sudo apt install python3 python3-pip
 pip3 install python-telegram-bot 
-pip3 install mysql.connector
 pip3 install mysql-connector-python
 sudo apt install mysql-server
+if [$? -ne 0]; then sudo apt install mariadb-server fi
 sudo mysql_secure_installation
 sudo mysql -u root -p -e " CREATE DATABASE bot; CREATE USER 'bot'@'localhost' IDENTIFIED BY 'sc8KCek9aaVf3Fnn'; "
 sudo mysql -u root -p bot < bot.sql
